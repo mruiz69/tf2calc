1ed05395e91836b7d9737529409c867c094b83d2
mruiz69 <m.ruiz69@alumnos.santotomas.cl>
2024-10-09 21:14:19 -0300

    Se agrega funcionalidad de mapa

--- app/src/main/java/com/example/applayouts/MainActivity.java
+++ app/src/main/java/com/example/applayouts/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.applayouts;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
+
+
 
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btVolver = findViewById(R.id.buttonMap);
+        btVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Mapa.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }

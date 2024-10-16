e7de7111f64fc26de349d3f72b6f4e7cc53a3dcb
mruiz69 <m.ruiz69@alumnos.santotomas.cl>
2024-10-16 20:18:43 -0300

    Se agrega boton para seleccionar imagen de perfil en menu principal

--- app/src/main/java/com/example/applayouts/MainActivity.java
+++ app/src/main/java/com/example/applayouts/MainActivity.java
@@ -1,21 +1,47 @@
 package com.example.applayouts;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
 
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Button btnConvertir;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private RecyclerView recyclerView;
+    private ImageAdapter imageAdapter;
+    private List<Uri> imageUriList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btVolver = findViewById(R.id.buttonMap);
+
+        Button btnSelectImage = findViewById(R.id.btnSelectImage);
+        recyclerView = findViewById(R.id.recyclerView);
+        // Configurar el RecyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        imageAdapter = new ImageAdapter(this, imageUriList);
+        recyclerView.setAdapter(imageAdapter);
+        // Evento para seleccionar imágenes
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
         btVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,5 +50,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Método para seleccionar imágenes desde la galería
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            imageUriList.add(imageUri); // Añadir la imagen seleccionada a la lista
+            imageAdapter.notifyDataSetChanged(); // Notificar al adaptador para actualizar el RecyclerView
+        }
+    }
+
 
 }
